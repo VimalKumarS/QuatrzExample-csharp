d4ad96849185f5c6fee62051f43840819ffbca55
VIMALKUMARPC\vimal <vimalkumarsinghal@gmail.com>
2018-10-14 08:24:10 -0700

    Quartz csharp example

---
+++ ConsoleApp1/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+
+namespace ConsoleApp1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        //{
+
+        //}
+        //public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        //{
+        //    // scaffolded code...
+        //}
+
+        public void Configure(
+    IApplicationBuilder app,
+    IHostingEnvironment env,
+    ILoggerFactory loggerFactory,
+    IApplicationLifetime lifetime) // added this parameter
+        {
+            // the following 3 lines hook QuartzStartup into web host lifecycle
+            var quartz = new QuartzStartup();
+            lifetime.ApplicationStarted.Register(quartz.Start);
+            lifetime.ApplicationStopping.Register(quartz.Stop);
+
+            // .... original scaffolded code here ....
+        }
+    }
+}
+//https://github.com/quartznet/quartznet/tree/master/src/Quartz.Examples
